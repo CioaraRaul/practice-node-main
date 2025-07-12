1a02b8940519c7b5e28066b57ba824d8b72ccb07
CioaraRaul <cioararaul08@gmail.com>
2025-07-12 08:31:04 +0300

    added nav

--- mvc/00-starting-setup/routes/admin.js
+++ mvc/00-starting-setup/routes/admin.js
@@ -3,14 +3,15 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
-const { getAddProduct } = require("../controllers/products");
-const products = require("../controllers/products");
+const { getAddProduct } = require("../controllers/admin");
+const products = require("../controllers/admin");
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", getAddProduct);
 
+router.get("/products", products.getProducts);
 // /admin/add-product => POST
 router.post("/add-product", products.postAddProduct);
 
