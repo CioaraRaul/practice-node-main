8a09b3e74ba900b7ff1d335cf8519a80596674ec
CioaraRaul <cioararaul08@gmail.com>
2025-07-14 00:43:07 +0300

    updating editing prod

--- dynamic_routes_and_advanced_models/00-starting-setup/controllers/admin.js
+++ dynamic_routes_and_advanced_models/00-starting-setup/controllers/admin.js
@@ -4,6 +4,8 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false,
+    product: {},
   });
 };
 
@@ -12,16 +14,27 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
 
 exports.getEditProduct = (req, res, next) => {
-  res.render("admin/edit-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    editing: true,
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.produtId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
@@ -34,3 +47,5 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postEditProduct = (req, res, next) => {};
